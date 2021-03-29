92e3c24f2d4a2452f4e4974163aadfb6445f2883
MrSkyGold <devmarcos.py@gmail.com>
2021-03-29 16:16:24 -0300

    host - todos

--- gestao_web/settings.py
+++ gestao_web/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '13mb6^c(4f&!$fcjdu-pahv9nfq^aa^$nkc_0l*z6z@r@ndf4g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
